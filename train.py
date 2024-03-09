c0620e2eed22b306a43f1d4f9c7b7bc0c41a415e
agurel33 <agurel@ucsd.edu>
2024-03-09 12:46:37 -0800

    Update train.py

--- train.py
+++ train.py
@@ -1,1 +1,14 @@
 # train.py is the main file to train the model
+#for one game:
+initialize model 
+initialize opponent (either itself, other chess bot, past version of itself?)
+
+initialize blank chess board
+
+coin flip decide black/white players
+
+inputs to model look like: (bit for black or white, known board, turn count, opponent's seen board)
+                            
+initiliaze opponent's seen board (piece, location, # turn seen, bit for captured or not)
+
+while both kings are present on the board:
