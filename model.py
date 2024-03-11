7baf6609bc7937417f1373e082d60f84f2c41500
agurel33 <agurel@ucsd.edu>
2024-03-11 15:56:35 -0700

    Update model.py

--- model.py
+++ model.py
@@ -1,12 +1,23 @@
-# Model Inputs:
-#  1 ) Black / White  
-#  2 ) 100% Known Board
-#  3 ) Turn Count
-#  4 ) Opponent's seen board 
-#           - starts at initial chess game positions
-#           - updates new positions as they are revealed
+import torch as torch
+import torch.nn as nn
+import torch.nn.functional as F
+import tensorflow as tf
+from tensorflow.keras import layers
 
+class DQN(tf.keras.Model):
+    def __init__(self):
+        #worry about which layers to use here
+        self.flatten = layers.Flatten()
 
-#
-# Model Outputs:
-#  1 ) Next Move
+        self.dense1 = layers.Dense(256, activation='relu')
+        self.dense2 = layers.Dense(128, activation='relu')
+        self.dense3 = layers.Dense(64, activation='relu')
+        self.output = layers.Dense(1, activation=None)
+
+    def forward(self, state):
+        x = self.flatten(state)
+        x = self.dense1(x)
+        x = self.dense2(x)
+        x = self.dense3(x)
+        value = self.output(x)
+        return value
